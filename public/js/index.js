37176d7290a3fe5560bbeac7373c33758b392537
Dara Nhep <dara.h.nhep@gmail.com>
2016-12-26 19:57:01 -0600

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+let socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Jen',
+    text: 'Hey. this is Jen.'
+  });
+
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function (email) {
+  console.log(email);
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
