e6ad263532afe562bc1a6f5df0cfa6217e736c54
Dara Nhep <dara.h.nhep@gmail.com>
2016-12-26 20:06:29 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Jen',
-    text: 'Hey. this is Jen.'
-  });
-
 });
 
 socket.on('disconnect', function () {
